7b6593858828106332ea689158c1e2710defc023
im-number3 <73306464+im-number3@users.noreply.github.com>
2020-11-04 15:39:13 +0100

    recherche

--- nodejsfile.js
+++ nodejsfile.js
@@ -51,8 +51,9 @@ MongoClient.connect('mongodb://localhost:27017', (err, db) => {
     });
   });
   
+  
   app.get('/searchincident', (req, res) => {
-	let searchWords = req.body.search_words;
+	let searchWords = req.query.search_words;
 	dbo.collection('incidents').find({$text:{$search:searchWords}}).toArray((err, doc) => {
       if (err) throw err;
       if(req.session.username!=null){
